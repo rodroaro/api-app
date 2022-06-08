732b3b4f088a73ed47ddf91a9142f636bb3dcd9f
rodroaro <rod_roaro_13@hotmail.com>
2022-06-07 19:45:54 -0500

    Handle Authentication error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,18 @@ class ApplicationController < ActionController::API
       'JsonapiErrorsHandler::Errors::NotFound'
   )
   rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
+
+  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+
+  private
+
+  def authentication_error
+    error = {
+      'status': '401',
+      'source': { 'pointer': '/code' },
+      'title': 'Authentication code is invalid',
+      'detail': 'You must provide valid code in order to exchange it for token.'
+    }
+    render json: { "errors": [error] }, status: 401
+  end
 end
--- spec/requests/access_tokens_spec.rb
+++ spec/requests/access_tokens_spec.rb
@@ -1,7 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe "AccessTokens", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe AccessTokensController, type: :controller do
+  describe '#create' do
+    context 'when invalid request' do
+      let(:error) do
+        {
+          'status': '401',
+          'source': { 'pointer': '/code' },
+          'title': 'Authentication code is invalid',
+          'detail': 'You must provide valid code in order to exchange it for token.'
+        }
+      end
+
+      subject { post :create }
+
+      it 'should return 401 status code' do
+        subject
+        expect(response).to have_http_status(401)
+      end
+
+      it 'should return proper error body' do
+        subject
+        expect(json[:errors]).to include(error)
+      end
+    end
+
+    context 'when success request' do
+    end
   end
 end
