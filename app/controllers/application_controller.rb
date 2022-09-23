018ba88472ded4d7cb364bddf09565917856b755
rodroaro <rod_roaro_13@hotmail.com>
2022-09-23 17:31:43 -0500

    Handle unauthorized request for logging out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  class AuthorizationError < StandardError; end
   include JsonapiErrorsHandler
 
   ErrorMapper.map_errors!(
@@ -8,6 +9,7 @@ class ApplicationController < ActionController::API
   rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
 
   rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+  rescue_from AuthorizationError, with: :authorization_error
 
   private
 
@@ -19,5 +21,15 @@ class ApplicationController < ActionController::API
       'detail': 'You must provide valid code in order to exchange it for token.'
     }
     render json: { "errors": [error] }, status: 401
+  end
+
+  def authorization_error
+    error = {
+      'status': '403',
+      'source': { 'pointer': '/headers/authorization' },
+      'title': 'Not authorized',
+      'detail': 'You have no right to access this resource.'
+    }
+    render json: { "errors": [error] }, status: 403
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   post 'login', to: 'access_tokens#create'
+  delete 'logout', to: 'access_tokens#destroy'
   resources :articles, only: %i[index show]
 end
